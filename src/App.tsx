dfcfbe55b45896b11587b5f21afd7794c874fd90
Christian Caracach <christiancaracach@gmail.com>
2025-04-06 21:36:13 -0300

    react analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const heroRef = useRef<HTMLElement>(null);
