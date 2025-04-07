860b82ffb04cdc03c5406f6f7435266d4943ef9b
Christian Caracach <christiancaracach@gmail.com>
2025-04-06 21:18:51 -0300

    Retoques finales

--- src/App.tsx
+++ src/App.tsx
@@ -33,11 +33,30 @@ function App() {
     <div className="app-container">
       <section ref={heroRef} className="hero-section">
         <div className="neo-input-container">
-          <input
-            type="text"
-            className="neo-input"
-            placeholder="Que necesitas?"
-          />
+          <form
+            action="https://formsubmit.co/robiduvalentin@gmail.com"
+            method="POST"
+            className="neo-input-form"
+          >
+            <input
+              type="text"
+              name="necesidad"
+              className="neo-input"
+              placeholder="Que necesitas?"
+            />
+            <input
+              type="text"
+              name="quien"
+              className="neo-input"
+              placeholder="Quien sos?"
+            />
+            <button type="submit" className="neo-button">
+              <span className="neo-button-text">Enviar</span>
+              <span className="neo-button-icon">
+                <i className="fas fa-paper-plane"></i>
+              </span>
+            </button>
+          </form>
         </div>
       </section>
 
@@ -49,14 +68,34 @@ function App() {
 
       <section className="social-section">
         <div className="social-icons">
-          <a href="#" className="social-icon">
+          <a
+            href="https://www.instagram.com/valentinrobidu/"
+            className="social-icon"
+            target="_blank"
+          >
             <i className="fab fa-instagram"></i>
           </a>
-          <a href="#" className="social-icon">
-            <i className="fab fa-twitter"></i>
+          <a
+            href="/Robidu_Presskit_2025.pdf"
+            className="social-icon"
+            download="Robidu_Presskit_2025.pdf"
+            target="_blank"
+          >
+            <i className="fab fa-download"></i>
           </a>
-          <a href="#" className="social-icon">
-            <i className="fab fa-linkedin"></i>
+          <a
+            href="https://www.youtube.com/watch?v=Oc7lzbMNW8Q"
+            className="social-icon"
+            target="_blank"
+          >
+            <i className="fab fa-youtube"></i>
+          </a>
+          <a
+            href="https://soundcloud.com/robiduteama/robidu-full-set-live/s-cc67szWIiRp?si=4a6921feb016461ab085253d8ea9a9a0&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+            className="social-icon"
+            target="_blank"
+          >
+            <i className="fab fa-soundcloud"></i>
           </a>
         </div>
       </section>
